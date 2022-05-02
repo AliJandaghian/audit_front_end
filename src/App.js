58637d8f8cd4ce4e7140b7ea3b20032e7a99bd07
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-05-02 12:05:19 -0700

    Add Config module to project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import NavBar from "./components/navBar";
 import { Route, Redirect, Routes } from "react-router-dom";
 import Audits from "./components/audits";
-
+import { ToastContainer } from "react-toastify";
 
 class App extends Component {
   render() {
   return (
     <React.Fragment>
       <NavBar />
+      <ToastContainer />
       <Audits />
     </React.Fragment>
   );
