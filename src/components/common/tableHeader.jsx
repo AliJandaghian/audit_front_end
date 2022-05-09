d1c75077668a67fbb0631174ba45b7881fed5e8e
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-05-09 16:45:19 -0700

    Add reusable table header to common

---
+++ src/components/common/tableHeader.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  raiseSort = (path) => {
+    let sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    else sortColumn = { path, order: "asc" };
+    this.props.onSort(sortColumn);
+  };
+
+  renderSortIcon = (col) => {
+    let sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path !== col.path || !col.path) return null;
+    if (sortColumn.order === "asc") return <i class="fas fa-angle-up"></i>;
+    return <i class="fas fa-angle-down"></i>;
+  };
+  render() {
+    const { columns } = this.props;
+    return (
+      <thead>
+        <tr>
+          {columns.map((col) => (
+            <th
+              key={col.path || col.key}
+              scope = 'col'
+              className="clickable"
+              onClick={() => this.raiseSort(col.path)}
+            >
+              {col.label} {this.renderSortIcon(col)}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
