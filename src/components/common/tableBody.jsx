a7f80053f5cec081425fd51ebb46fce9d20fdbdc
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-05-09 19:45:51 -0700

    Add reusable table body to common

---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import _ from "lodash";
+
+class TableBody extends Component {
+  renderCell(item, col) {
+    if (col.content) return col.content(item);
+    return _.get(item, col.path);
+  }
+
+  generateKey(item, col) {
+    return item._id + (col.path || col.key);
+  }
+
+  render() {
+    const { columns, data } = this.props;
+    return (
+      <tbody>
+        {data.map((item) => (
+          <tr key={item._id} scope="row">
+            {columns.map((col) => (
+              <td key={this.generateKey(item, col)}>
+                {this.renderCell(item, col)}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
