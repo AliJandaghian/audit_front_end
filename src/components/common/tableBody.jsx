d3c44f3159fefd473bc9e440c53357090af636b0
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-05-12 09:45:08 -0700

    Removed scope from table body that was added by mistake

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -16,7 +16,7 @@ class TableBody extends Component {
     return (
       <tbody>
         {data.map((item) => (
-          <tr key={item._id} scope="row">
+          <tr key={item._id}>
             {columns.map((col) => (
               <td key={this.generateKey(item, col)}>
                 {this.renderCell(item, col)}
