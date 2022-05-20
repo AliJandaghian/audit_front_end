3b6725f68d746863b605002194afdbfa9ba06921
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-05-20 14:50:54 -0700

    Add audit Table to components

---
+++ src/components/auditTable.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import Table from "./common/table";
+
+class AuditTable extends Component {
+  formatDate = (auditDate, dateOptions) => {
+    return (
+      <p>
+        {new Date(Date.parse(auditDate)).toLocaleString("en-US", dateOptions)}
+      </p>
+    );
+  };
+  columns = [
+    {
+      path: "auditDate",
+      label: "Date",
+      content: (audit) => {
+        return this.formatDate(audit.auditDate, {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+      },
+    },
+    {
+      key: "auditTime",
+      label: "Time",
+      content: (audit) => {
+        return this.formatDate(audit.auditDate, {
+          hour: "numeric",
+          minute: "numeric",
+        });
+      },
+    },
+    { path: "auditor.name", label: "Auditor" },
+    { path: "machine.name", label: "Machine" },
+    {
+      path: "defects",
+      label: "Defects",
+      content: (audit) => {
+        return <div>{audit.defects.map((def) => (
+          <span
+            key={audit._id + def._id}
+            className="span-badge span-badge__red"
+          >
+            {def.name}
+          </span>
+        ))}</div>
+      },
+    },
+    {
+      key: "edit",
+      label: "Edit",
+      content: (item) => {
+        return (
+          <button
+            className="button edit-button"
+            onClick={() => this.props.onEdit(item._id)}
+          >
+            <i className="fas fa-pen"></i>
+          </button>
+        );
+      },
+    },
+    {
+      key: "delete",
+      label: "Delete",
+      content: (item) => {
+        return (
+          <button
+            className="button delete-button"
+            onClick={() => {if(window.confirm('Delte the item?')) { this.props.onDelete(item._id)}}}
+          >
+            <i className="fas fa-trash-alt"></i>
+          </button>
+        );
+      },
+    },
+  ];
+  render() {
+    const { audits, onSort, sortColumn } = this.props;
+    return (
+      <Table
+        data={audits}
+        columns={this.columns}
+        onSort={onSort}
+        sortColumn={sortColumn}
+      />
+    );
+  }
+}
+
+export default AuditTable;
