6eeed1999812f3f3d64c62d23920ece2d1c058b1
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-05-20 14:46:14 -0700

    Add machine service to services

---
+++ src/services/machineService.js
@@ -0,0 +1,30 @@
+
+import http from "./httpService";
+
+const apiEndpoint = "/machines";
+
+function machineUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
+export async function getMachine(defectId) {
+  return await http.get(machineUrl(defectId));
+}
+
+export async function getMachines() {
+  return await http.get(apiEndpoint);
+}
+
+export async function saveMachines(machine) {
+  const machineId = machine._id;
+  if (machineId) {
+    const body = { ...machine };
+    delete body._id;
+    return http.put(machineUrl(machineId), body);
+  }
+  return await http.post(apiEndpoint, machine);
+}
+
+export async function deleteDefect(machineId) {
+  return await http.delete(machineUrl(machineId));
+}
