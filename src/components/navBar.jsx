0525d0f62d0cc4861da3ffc8ab0a77f5eae04c43
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-06-08 09:08:05 -0700

    Add user access level to navbar items

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -11,34 +11,43 @@ class NavBar extends Component {
   };
 
   render() {
+    const {user}=this.props
     return (
       <div className="nav">
+        <div className="nav_brand_container">
         <a className="nav__brand link " href="/">
           Auditing App
         </a>
-
+        <span className="nav_department" >
+          {user?.department?.name}
+        </span>
+        </div>
         <button onClick={this.handleExpandButton} className="nav__toggler">
           {this.state.navStatus && <i className="fas fa-times"></i> ||<i className="fas fa-bars"></i> }
         </button>
         <ul className={`list nav__list ${this.state.navStatus}`}>
-          <li className="nav__item">
+          {user?.isManager && <li className="nav__item">
             <a className="link" href="/">
               Design new Audit
             </a>
-          </li>
-          <li className="nav__item">
-            <a className="link" href="/">
+          </li>}
+         { user?.isManager &&<li className="nav__item">
+            <a className="link" href="/settings">
               Setting
             </a>
-          </li>
-          <li className="nav__item">
-            <a className="link" href="/">
+          </li>}
+          {user && <li className="nav__item">
+            <a className="link" href="/logout">
               Log Out
             </a>
-          </li>
-          <li className="nav__item">
-            <button className="link user_icon">UN</button>
-          </li>
+          </li> || <li className="nav__item">
+            <a className="link" href="/signup">
+            Sign up
+            </a>
+          </li>}
+         {user && <li className="nav__item">
+            <button className="link user_icon">{user.name?.charAt(0)}</button>
+          </li>}
         </ul>
       </div>
     );
