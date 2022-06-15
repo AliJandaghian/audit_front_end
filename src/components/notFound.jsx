74ba9ed38c8da6b79c4db9da7d7f515e5aba484c
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-06-15 11:29:49 -0700

    Add not-Found page

---
+++ src/components/notFound.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    state = {  } 
+    render() { 
+        return (<h2 className="Not-Found-Page">Sorry! We Couldn't Find that Page.</h2>);
+    }
+}
+ 
+export default NotFound;
