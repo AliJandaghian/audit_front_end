0a83a5367a6baeaeef148e446410420cb411eaba
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-06-15 11:11:39 -0700

    Clean and make pretty the code of NavBar.

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -11,43 +11,49 @@ class NavBar extends Component {
   };
 
   render() {
-    const {user}=this.props
+    const { user } = this.props;
     return (
       <div className="nav">
         <div className="nav_brand_container">
-        <a className="nav__brand link " href="/">
-          Auditing App
-        </a>
-        <span className="nav_department" >
-          {user?.department?.name}
-        </span>
+          <a className="nav__brand link " href="/">
+            Auditing App
+          </a>
+          <span className="nav_department">{user?.department?.name}</span>
         </div>
         <button onClick={this.handleExpandButton} className="nav__toggler">
-          {this.state.navStatus && <i className="fas fa-times"></i> ||<i className="fas fa-bars"></i> }
+          {(this.state.navStatus && <i className="fas fa-times"></i>) || (
+            <i className="fas fa-bars"></i>
+          )}
         </button>
         <ul className={`list nav__list ${this.state.navStatus}`}>
-          {user?.isManager && <li className="nav__item">
-            <a className="link" href="/">
-              Design new Audit
-            </a>
-          </li>}
-         { user?.isManager &&<li className="nav__item">
-            <a className="link" href="/settings">
-              Setting
-            </a>
-          </li>}
-          {user && <li className="nav__item">
-            <a className="link" href="/logout">
-              Log Out
-            </a>
-          </li> || <li className="nav__item">
-            <a className="link" href="/signup">
-            Sign up
-            </a>
-          </li>}
-         {user && <li className="nav__item">
-            <button className="link user_icon">{user.name?.charAt(0)}</button>
-          </li>}
+          {user?.isManager && (
+            <li className="nav__item">
+              <a className="link" href="/">
+                Design new Audit
+              </a>
+            </li>
+          )}
+          {user?.isManager && (
+            <li className="nav__item">
+              <a className="link" href="/settings">
+                Setting
+              </a>
+            </li>
+          )}
+          {(user && (
+            <li className="nav__item">
+              <a className="link" href="/logout">
+                Log Out
+              </a>
+            </li>
+          )) || (
+            <li className="nav__item">
+              <a className="link" href="/signup">
+                Sign up
+              </a>
+            </li>
+          )}
+          {user && <li className="nav__item user_icon">{user.name}</li>}
         </ul>
       </div>
     );
